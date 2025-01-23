6721b6e22228917615a6fa55c4729ec0b4d7d02e
Sergei Danilov <sergeidanilov@gmail.com>
2025-01-22 21:14:55 -0800

    feat: add correct parsing of the multiple Checks tables located horizontally on the same level

--- main.py
+++ main.py
@@ -81,96 +81,150 @@ def parse_single_global_account_number(line: str) -> str | None:
     return None
 
 
-def extract_with_pdf_plumber(pdf_path: str) -> Statement:
-    sections: List[Section] = []
-    current_section_transactions: List[Transaction] = []
-    current_section_name: str | None = None
-    filename = os.path.basename(pdf_path)
-    date_match_filename = re.search(r"eStmt_(\d{4}-\d{2}-\d{2})", filename)
-    if date_match_filename:
-        statement_date = date_match_filename.group(1)
-    else:
-        statement_date = "Unknown Date"
+class StatementParser:
+    def __init__(self, pdf_path: str):
+        self.pdf_path = pdf_path
+        self.is_section_has_substrings = False
+        self.current_section_transactions: List[Transaction] = []
+        self.section_started = False
 
-    account_number = None  # Initialize account_number
+    def extract_with_pdf_plumber(self) -> Statement:
+        sections: List[Section] = []
 
-    with pdfplumber.open(pdf_path) as pdf:
-        is_in_date_range = False
-        previous_line: str | None = None
-        for page in pdf.pages:
-            text = page.extract_text()
-            for line in text.splitlines():
-                # global account number is single per statement and
-                # always must exist
-                global_account_number = parse_single_global_account_number(
-                    line)
-                if global_account_number:
-                    account_number = global_account_number
-                # In case of combibed statement override global account number
-                # with number from combined statement
-                if line.startswith("Account number:"):
-                    parsed_account_number = parse_combibed_account_number(line)
-                    if parsed_account_number:
-                        account_number = parsed_account_number
-                if is_in_date_range:
-                    if line.startswith("Total"):
-                        is_in_date_range = False
-                        if current_section_name:
-                            sections.append(
-                                Section(
-                                    current_section_name,
-                                    current_section_transactions,
-                                    account_number
+        current_section_name: str | None = None
+        filename = os.path.basename(self.pdf_path)
+        date_match_filename = re.search(r"eStmt_(\d{4}-\d{2}-\d{2})", filename)
+        if date_match_filename:
+            statement_date = date_match_filename.group(1)
+        else:
+            statement_date = "Unknown Date"
+
+        account_number = None  # Initialize account_number
+
+        with pdfplumber.open(self.pdf_path) as pdf:
+            previous_line: str | None = None
+            for page in pdf.pages:
+                text = page.extract_text()
+                for line in text.splitlines():
+                    # global account number is single per statement and
+                    # always must exist
+                    global_account_number = parse_single_global_account_number(
+                        line)
+                    if global_account_number:
+                        account_number = global_account_number
+                    # In case of combibed statement override global account
+                    # number with number from combined statement
+                    if line.startswith("Account number:"):
+                        parsed_account_number = parse_combibed_account_number(
+                            line)
+                        if parsed_account_number:
+                            account_number = parsed_account_number
+                    if self.section_started:
+                        if line.startswith("Total"):
+                            self.section_started = False
+                            if current_section_name:
+                                sections.append(
+                                    Section(
+                                        current_section_name,
+                                        self.current_section_transactions,
+                                        account_number
+                                    )
                                 )
-                            )
-                            current_section_transactions = []
-                            current_section_name = None
+                                self.current_section_transactions = []
+                                current_section_name = None
+                            continue
+                        sublines = self.find_possible_substrings(line)
+                        # handle case when two check tables posted horizontally
+                        # next to each other
+                        for subline in sublines:
+                            self.parse_single_transaction(subline)
                         continue
-                    date_match = date_pattern.search(line)  # Moved line
+                    else:
+                        new_section_started = line.startswith(
+                            "Date") and line.endswith("Amount")
+                        if new_section_started:
+                            # handle case when table continues on another page
+                            if previous_line and \
+                                    previous_line.endswith(" - continued"):
+                                previous_line = previous_line.replace(
+                                    " - continued", "")
+                            current_section_name = previous_line
+                            self.section_started = True
+                            self.is_section_has_substrings = \
+                                self.check_section_has_substrings(line)
+                    date_match = date_pattern.search(line)
                     if date_match:
-                        parts = line.split()
-                        date = parts[0]
-                        amount = parts[-1]
-                        description_parts = parts[1:-1]
-                        description = " ".join(description_parts)
-                        transaction = Transaction(date, description, amount)
-                        current_section_transactions.append(transaction)
-                    elif (
-                            is_in_date_range
-                            and not date_match):
-                        # line is continuation of the prev date line
-                        # 07/24/19 compant payroll $100
-                        # CO ID:CXXXXXXXXX WEB
-                        # Concatenate such lines
-                        if len(current_section_transactions) == 0:
-                            print(
-                                "Can not create multiline transaction for "
-                                "empty transaction list")
-                            sys.exit(1)
-                        last_transaction = current_section_transactions[-1]
-                        last_transaction.description += " " + line
-                        continue
-                else:
-                    new_section_started = line.startswith(
-                        "Date") and line.endswith("Amount")
-                    if new_section_started:
-                        if previous_line and \
-                                previous_line.endswith(" - continued"):
-                            previous_line = previous_line.replace(
-                                " - continued", "")
-                        current_section_name = previous_line
-                        is_in_date_range = True
-                date_match = date_pattern.search(line)
-                if date_match:
-                    previous_line = line
-                elif is_in_date_range:
-                    # Keep the previous_line as is if in date range
-                    # but no date_match
-                    pass
-                else:
-                    # Update previous_line normally when not in date range
-                    previous_line = line
-    return Statement(statement_date, sections)
+                        previous_line = line
+                    elif self.section_started:
+                        # Keep the previous_line as is if in date range
+                        # but no date_match
+                        pass
+                    else:
+                        # Update previous_line normally when not in date range
+                        previous_line = line
+        return Statement(statement_date, sections)
+
+    def parse_single_transaction(self, line: str):
+        date_match = date_pattern.search(line)
+        if date_match:
+            parts = line.split()
+            date = parts[0]
+            amount = parts[-1]
+            description_parts = parts[1:-1]
+            description = " ".join(description_parts)
+            transaction = Transaction(
+                date, description, amount)
+            self.current_section_transactions.append(transaction)
+        elif (
+                self.section_started
+                and not date_match):
+            # line is continuation of the prev date line
+            # 07/24/19 compant payroll $100
+            # CO ID:CXXXXXXXXX WEB
+            # Concatenate such lines
+            if len(self.current_section_transactions) == 0:
+                print(
+                    "Can not create multiline transaction for "
+                    "empty transaction list")
+                sys.exit(1)
+            last_transaction = self.current_section_transactions[-1]
+            last_transaction.description += " " + line
+
+    def find_possible_substrings(self, line: str) -> List[str]:
+        date_matches = list(date_pattern.finditer(line))
+        if len(date_matches) <= 1:
+            return [line]
+        if len(date_matches) > 2:
+            print(
+                "Line has more than 2 duplicates."
+                f"It's not supported yet {line}")
+            sys.exit(1)
+            # print(f"Date: {match.group(0)}, Index: {match.start()}")
+        second_date_index = date_matches[1].start()
+        line1 = line[:second_date_index].strip()
+        line2 = line[second_date_index:].strip()
+        return [line1, line2]
+
+    def check_section_has_substrings(self, line: str) -> bool:
+        amount_indices = [i for i in range(
+            len(line)) if line.startswith('Amount', i)]
+        date_indices = [i for i in range(
+            len(line)) if line.startswith('Date', i)]
+
+        if len(date_indices) < 2:
+            return False
+
+        if len(amount_indices) > 2 and len(date_indices) > 2:
+            print(
+                "Line has more than 2 duplicates."
+                f"It's not supported yet {line}")
+            sys.exit(1)
+
+        second_date_index = date_indices[1]
+
+        part1 = line[:second_date_index].strip()
+        part2 = line[second_date_index:].strip()
+        return part1 == part2
 
 
 class FileWriterClass:
@@ -219,13 +273,13 @@ if __name__ == "__main__":
         "/home/sdanilov/Build/PDF-Scraper-for-Bank-of-America-Statements/"
         "data/out"
     )
-
     if args.statement:
-        statement = extract_with_pdf_plumber(args.statement)
+        statement = StatementParser(args.statement).extract_with_pdf_plumber()
         file_writer.append_statement(statement)
     elif args.dir:
         for filename in os.listdir(args.dir):
             if filename.endswith(".pdf"):
                 pdf_path = os.path.join(args.dir, filename)
-                statement = extract_with_pdf_plumber(pdf_path)
+                statement = StatementParser(
+                    pdf_path).extract_with_pdf_plumber()
                 file_writer.append_statement(statement)
--- test_parse_account_number.py
+++ test_parse_account_number.py
@@ -1,5 +1,6 @@
 from main import parse_combibed_account_number
 from main import parse_single_global_account_number
+from main import StatementParser
 
 
 def test_parse_account_number():
@@ -23,6 +24,26 @@ def test_parse_single_global_account_number():
     print("Unit test for parse_account_number passed!")
 
 
+def test_check_table_duplicated():
+    test_line = "Date Check # Amount Date Check # Amount"
+    parser = StatementParser("")
+
+    res = parser.check_section_has_substrings(test_line)
+    assert res is True, f"Expected True, but got: {res}"
+    print("Unit test for check_table_duplicated passed!")
+
+
+def test_find_duplicated_strings():
+    test_line = "06/11/19 193 -15,750.00 06/11/19 194 -260.00"
+    parser = StatementParser("")
+    res = parser.find_possible_substrings(test_line)
+    expected_res = ['06/11/19 193 -15,750.00', '06/11/19 194 -260.00']
+    assert res == expected_res, \
+        f"Expected res to be {expected_res}, but got {res}"
+
+
 if __name__ == "__main__":
     test_parse_account_number()
     test_parse_single_global_account_number()
+    test_check_table_duplicated()
+    test_find_duplicated_strings()
